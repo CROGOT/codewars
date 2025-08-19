0dc7df376bcbc6de6cc7e9f06e653e1430544cc0
CROGOT <jekafokin@gmail.com>
2025-08-19 13:08:01 +0300

    иксбоначи и римские цифры

---
+++ java/Fibonacci_Tribonacci_and_friends.java
@@ -0,0 +1,21 @@
+//https://www.codewars.com/kata/556e0fccc392c527f20000c5/train/java
+//
+
+import java.util.Arrays;
+
+public class Fibonacci_Tribonacci_and_friends {
+  
+  public static void main(String[] args) {
+    Xbonacci variabonacci = new Xbonacci();
+    System.out.println(Arrays.toString(variabonacci.xbonacci(new double []{1,0,0,0,0,0,1},10)));
+  }
+
+}
+
+public class Xbonacci {
+
+  public double[] xbonacci(double[] signature, int n) {
+      double[] out = new double[n];
+      return out;
+  }
+} 
